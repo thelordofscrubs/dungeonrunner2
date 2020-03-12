6364b85fa39305df3f54af4189031d6136ed1f52
thelordofscrubs <ekaituri@gmail.com>
2020-03-12 18:40:24 -0500

    stuff

--- scripts/levelScript.gd
+++ scripts/levelScript.gd
@@ -14,6 +14,93 @@ func _ready():
 
 func fireProjTest(vin):
 	add_child(Arrow.new(player.coordinates + vin * 10, vin, Vector2(0,0)))
+	print("fired projectile")
+
+func startLevel(id):
+#	print("id:"+str(id))
+#	levelID = int(id)
+#	name = "level"+str(levelID)
+	set_pause_mode(1)
+#	var levelName = "res://maps/map"+str(levelID)+"TileMap.tscn"
+#	print(levelName)
+	levelTileMap = load("res://maps/map0TileMap.tscn").instance()
+	var x
+	var y
+	var i = 0
+	while x != -1:
+		x = levelTileMap.get_cell(i,0)
+		i += 1
+	x = i-1
+	i = 0
+	while y != -1:
+		y = levelTileMap.get_cell(0,i)
+		i += 1
+	y = i-1
+	levelDimensions = Vector2(x,y)
+	print(str(levelDimensions))
+	### NEW X AND Y FROM HERE ###
+	for y in range(levelDimensions[1]):
+		for x in range(levelDimensions[0]):
+			if levelTileMap.get_cell(x,y) == 6:
+				initPlayerCoords = Vector2(x,y)#need player coordinates for spawning sprites in correct locations
+				currentPlayerCoordinates = initPlayerCoords
+	for y in range(levelDimensions[1]):
+		for x in range(levelDimensions[0]):
+			var cc = Vector2(x,y)
+			match levelTileMap.get_cell(x,y):
+				-1:
+					levelGrid[cc] = "oob"
+				0:
+					levelGrid[cc] = "floor"
+				1:
+					levelGrid[cc] = "wall"
+				2:
+					levelGrid[cc] = "finish"
+				3:
+					levelGrid[cc] = "door"
+					doors[cc] = false
+				4:
+					levelGrid[cc] = "chest"
+					spawnChest("doubleArrow",cc)
+				5:
+					levelGrid[cc] = "chest"
+				6: #playerSpawn
+					levelGrid[cc] = "floor"
+					levelTileMap.set_cellv(cc,0)
+				7: #blueSlimeSpawn
+					levelGrid[cc] = "floor"
+					var facing = Vector2(0,-1)
+					spawnMonster("blueSlime",cc,initPlayerCoords,facing)
+					levelTileMap.set_cellv(cc,0)
+				8: #keySpawn
+					levelGrid[cc] = "key"
+					spawnKey(cc)
+					levelTileMap.set_cellv(cc,0)
+				9: #potSpawn
+					levelGrid[cc] = "pot"
+					spawnPot(cc)
+					levelTileMap.set_cellv(cc,0)
+				10: #batSkeletonSpawn
+					levelGrid[cc] = "floor"
+					var facing = Vector2(0,-1)
+					spawnMonster("batSkeleton",cc,initPlayerCoords,facing)
+					levelTileMap.set_cellv(cc,0)
+				12:
+					levelGrid[cc] = "floor"
+					var facing = Vector2(1,0)
+					spawnMonster("blueSlime",cc,initPlayerCoords,facing)
+					levelTileMap.set_cellv(cc,0)
+	print("initial player coordinates are: "+str(initPlayerCoords[0])+", "+str(initPlayerCoords[1]))
+	levelTileMap.set_position(levelTileMap.get_position()-initPlayerCoords*Vector2(16,16))
+	levelTileMap.set_z_index(0)
+	playerNode = Player.new(initPlayerCoords)
+	add_child(playerNode)
+	playerNode.genSprite()
+	graphicsContainerNode.add_child(levelTileMap)
+	#get_node("healthBar").set_position()
+	graphicsContainerNode.set_position(OS.get_window_size()/Vector2(2,2)-Vector2(8,8))
+	for monster in monsters:
+		monster.getMap(levelGrid)
 
 func movePlayer(dir, delta):
 	match dir:
