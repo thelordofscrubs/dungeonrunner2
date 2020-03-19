83657bf5745b31a7fe272af378275a2d0d1202d3
ZEUBERNERD <connersutton@live.com>
2020-03-19 15:45:42 -0500

    Changed how atlases are made and how they they are pulled from
     May need some analysis of atlas making methodologies
     (How to make graph in python?)

--- scripts/atlasHandler.gd
+++ scripts/atlasHandler.gd
@@ -1,13 +1,14 @@
 extends Object
 class_name AtlasHandler
 var atlas = preload("res://sprites/spriteAtlas.png")
-
+var w
 func _init(tex = null):
 	if (tex):
 		atlas = tex
+	w = atlas.get_width()/16
 
 func grab(idx = 0, size = Vector2(16,16)):
 	var text = AtlasTexture.new()
 	text.set_atlas(atlas)
-	text.set_region(Rect2(Vector2(idx%16 * 16,floor(idx/16) * 16),size))
+	text.set_region(Rect2(Vector2(idx%w * 16,floor(idx/w) * 16),size))
 	return text
--- sprites/atlas.py
+++ sprites/atlas.py
@@ -7,24 +7,37 @@ images = []
 files = os.listdir()
 
 for f in files:
-    if f.endswith(".png"):
+    if (f != "spriteAtlas.png" and f.endswith(".png")):
         images.append(Image.open(f))
+length = len(images)
+#trying to get dimentions to be near a square number, (ex 25 would be 5 by 5, but 42 would be 6 by 7)
+#gonna try and use the inner most multiples, if fail:
+#then trying to just use very near numbers
+root = math.sqrt(length)
+r = math.ceil(root)
+l = math.floor(length/r)
+r = math.ceil(length/l)
+succsess = False
+while (abs(r-root) <= .1*length):
+    if (l*r == length):
+        succsess = True
+        break
+    l = math.floor(length/r)
+    r = math.ceil(length/l)
+if succsess:
+    atd = [r,l]
+else:
+    atd = [math.ceil(root),math.ceil(root)]
 
-if len(images) < 16:
-    atd = [len(images), 1]
-
-atl = math.ceil(len(images)/16)
-atd = [len(images)%16, atl]
-
-atlas = Image.new("RGBA", (16*16,atd[1]*16))#.load()
+atlas = Image.new("RGBA", (atd[0]*16,atd[1]*16))#.load()
 #
 #for image in images:
 #    image = image.load()
 
-for y in range(atl-1):
-    for x in range(16):
+for y in range(atd[1]):
+    for x in range(atd[0]):
         #print("pasting image "+str(y*16+x)+ " at position "+str(x)+", "+str(y))
-        atlas.paste(images[y*16+x],(x*16,y*16))
+        atlas.paste(images[y*atd[0]+x],(x*16,y*16))
 #        for x1 in range(16):
 #            for y1 in range(16):
 #                atlas[x*16+x1,y*16+y1] = image[y*16+x][x1,y1]
@@ -35,11 +48,12 @@ if atd[0] != 0:
     left = atd[0]
 
 for x in range(left):
-    atlas.paste(images[x-left],(x*16,(atl-1)*16))
+    atlas.paste(images[x-left],(x*16,(atd[1])*16))
 #    for x1 in range(16):
 #        for y1 in range(16):
 #            atlas[x*16+x1,atl*16+y1] = image[y*16+x][x1,y1]
 
+#atlas.show()
 atlas.save("spriteAtlas.png")
 
 
